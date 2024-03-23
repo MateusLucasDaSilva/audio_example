36e3697c00afc8abb558d9de01b2a174928d2be2
MateusLucasDaSilva <mateuslukas505@gmail.com>
2024-03-22 23:13:06 -0300

    changues

--- lib/app/audio_service.dart
+++ lib/app/audio_service.dart
@@ -1,6 +1,7 @@
-import 'dart:developer';
+import 'dart:async';
 import 'dart:io';
 
+import 'package:ffmpeg_kit_flutter/ffmpeg_kit.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:permission_handler/permission_handler.dart';
@@ -9,7 +10,17 @@ class AudioService extends ChangeNotifier {
   final recorder = FlutterSoundRecorder();
   final player = FlutterSoundPlayer();
 
+  StreamController<String> streamInfo = StreamController(sync: true);
+
   File? file;
+
+  List<String> paths = [];
+  int count = 0;
+
+  void addElement(String path) {
+    paths.add(path);
+    notifyListeners();
+  }
 
   void init() {
     initPlayer();
@@ -31,11 +42,13 @@ class AudioService extends ChangeNotifier {
     player.setSubscriptionDuration(const Duration(milliseconds: 500));
   }
 
-  void startPlay() {
+  void startPlay(File? fileOther) {
     if (file == null) {
       return;
     }
-    player.startPlayer(fromDataBuffer: file!.readAsBytesSync());
+    player.startPlayer(
+        fromDataBuffer:
+            fileOther?.readAsBytesSync() ?? file!.readAsBytesSync());
   }
 
   void stopPlay() {
@@ -43,8 +56,9 @@ class AudioService extends ChangeNotifier {
     notifyListeners();
   }
 
-  Future startRecord() async {
-    await recorder.startRecorder(toFile: "audio");
+  Future startRecord({String? path}) async {
+    count++;
+    await recorder.startRecorder(toFile: path ?? 'audio$count');
     notifyListeners();
   }
 
@@ -52,14 +66,37 @@ class AudioService extends ChangeNotifier {
     final filePath = await recorder.stopRecorder();
     final fileAudio = File(filePath!);
     file = fileAudio;
-    log('Recorded file path: $filePath');
+    streamInfo.add('Audio gravado');
     notifyListeners();
+  }
+
+  Future<void> concatenarAudios() async {
+    try {
+      if (paths.isEmpty || paths.length < 2) {
+        streamInfo.add('grave mais audios');
+        return;
+      }
+      const outPATH =
+          '/data/user/0/com.example.audio_example/cache/concatenado';
+      '/data/user/0/com.example.audio_example/cache/concatenado';
+
+      final String comand =
+          "-i ${paths[0]} -i ${paths[1]} -filter_complex 'concat=n=2:v=0:a=1[a]' -map '[a]' -codec:a libmp3lame -qscale:a 2 $outPATH";
+      await FFmpegKit.execute(comand);
+      File fileConcatenado = File(outPATH);
+      startPlay(fileConcatenado);
+
+      streamInfo.add('Audio Concatenado${fileConcatenado.path}');
+    } catch (e) {
+      rethrow;
+    }
   }
 
   @override
   void dispose() {
     recorder.closeRecorder();
     player.closePlayer();
+    streamInfo.close();
     super.dispose();
   }
 }
--- lib/app/home_page.dart
+++ lib/app/home_page.dart
@@ -15,6 +15,7 @@ class _HomePageState extends State<HomePage> {
     super.initState();
     listenService();
     service.init();
+    listenInfo();
   }
 
   @override
@@ -37,70 +38,126 @@ class _HomePageState extends State<HomePage> {
     }
   }
 
+  void listenInfo() {
+    service.streamInfo.stream.listen((event) {
+      ScaffoldMessenger.of(context)
+          .showSnackBar(SnackBar(content: Text(event)));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.teal.shade700,
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(service.file?.path ?? ""),
-              StreamBuilder<RecordingDisposition>(
-                builder: (context, snapshot) {
-                  final duration = snapshot.hasData
-                      ? snapshot.data!.duration
-                      : Duration.zero;
+        body: SingleChildScrollView(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const SizedBox(
+                  height: 200,
+                ),
+                Column(
+                  children: service.paths
+                      .map(
+                        (e) => Text(
+                          e,
+                          style: const TextStyle(color: Colors.white),
+                        ),
+                      )
+                      .toList(),
+                ),
+                Text(
+                  service.file?.path ?? "",
+                  style: const TextStyle(color: Colors.red),
+                ),
+                StreamBuilder<RecordingDisposition>(
+                  builder: (context, snapshot) {
+                    final duration = snapshot.hasData
+                        ? snapshot.data!.duration
+                        : Duration.zero;
 
-                  String twoDigits(int n) => n.toString().padLeft(2, '0');
+                    String twoDigits(int n) => n.toString().padLeft(2, '0');
 
-                  final twoDigitMinutes =
-                      twoDigits(duration.inMinutes.remainder(60));
-                  final twoDigitSeconds =
-                      twoDigits(duration.inSeconds.remainder(60));
+                    final twoDigitMinutes =
+                        twoDigits(duration.inMinutes.remainder(60));
+                    final twoDigitSeconds =
+                        twoDigits(duration.inSeconds.remainder(60));
 
-                  return Text(
-                    '$twoDigitMinutes:$twoDigitSeconds',
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 50,
-                      fontWeight: FontWeight.bold,
+                    return Text(
+                      '$twoDigitMinutes:$twoDigitSeconds',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 50,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    );
+                  },
+                  stream: service.recorder.onProgress,
+                ),
+                const SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () async {
+                        if (service.recorder.isRecording) {
+                          await service.stopRecorder();
+                        } else {
+                          await service.startRecord();
+                        }
+                      },
+                      child: Icon(
+                        service.recorder.isRecording ? Icons.stop : Icons.mic,
+                        size: 100,
+                      ),
                     ),
-                  );
-                },
-                stream: service.recorder.onProgress,
-              ),
-              const SizedBox(height: 20),
-              ElevatedButton(
-                onPressed: () async {
-                  if (service.recorder.isRecording) {
-                    await service.stopRecorder();
-                  } else {
-                    await service.startRecord();
-                  }
-                },
-                child: Icon(
-                  service.recorder.isRecording ? Icons.stop : Icons.mic,
-                  size: 100,
+                    ElevatedButton(
+                      onPressed: () async {
+                        if (service.player.isPlaying) {
+                          service.stopPlay();
+                        } else {
+                          service.startPlay(null);
+                        }
+                      },
+                      child: Icon(
+                        service.player.isPlaying
+                            ? Icons.stop
+                            : Icons.play_arrow,
+                        size: 100,
+                      ),
+                    ),
+                  ],
                 ),
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              ElevatedButton(
-                onPressed: () async {
-                  if (service.player.isPlaying) {
-                    service.stopPlay();
-                  } else {
-                    service.startPlay();
-                  }
-                },
-                child: Icon(
-                  service.player.isPlaying ? Icons.stop : Icons.play_arrow,
-                  size: 100,
+                const SizedBox(
+                  height: 20,
                 ),
-              ),
-            ],
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () {
+                        service.addElement(service.file?.path ?? '');
+                      },
+                      child: const Icon(
+                        Icons.add,
+                        size: 100,
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        service.concatenarAudios();
+                      },
+                      child: const Icon(
+                        Icons.ac_unit_outlined,
+                        size: 100,
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            ),
           ),
         ));
   }
